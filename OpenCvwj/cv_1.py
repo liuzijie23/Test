6d8a1b619fd8bfcd39f268ad8c11bc00d1a3a5b1
liuzijie23 <31502425+liuzijie23@users.noreply.github.com>
2017-10-22 00:45:40 +0800

    修改
    修改图片路径

--- OpenCvwj/cv_1.py
+++ OpenCvwj/cv_1.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('C:\\Users\\22262\\Desktop\\xx.jpg',cv2.IMREAD_UNCHANGED)
+img = cv2.imread('你的图片路径',cv2.IMREAD_UNCHANGED)
 cv2.imshow('image',img)
 cv2.waitKey(0)&0xFF
 cv2.destroyWindow('image')
